aafeb54a8d24f2169378855ee9dcc8a6457f1e5f
deficts <diegoar7@live.com>
2019-10-28 12:57:46 -0600

    comenzando desarrollo

---
+++ app/src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <md-toolbar>
+      <h3 class="md-title">Default</h3>
+    </md-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+  }
+}
+</script>
+
+<style>
+
+</style>
