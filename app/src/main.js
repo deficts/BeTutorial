7e630c256f69603dab827ae77bf7883c7eda1a93
deficts <diegoar7@live.com>
2019-10-28 13:11:42 -0600

    agregando firebase

--- app/src/main.js
+++ app/src/main.js
@@ -6,6 +6,19 @@ import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default-dark.css'
+import firebsae from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBOQe7xSjY9txzQCDzC9Y9joRBY9T9CC6w",
+    authDomain: "betutorial-48e65.firebaseapp.com",
+    databaseURL: "https://betutorial-48e65.firebaseio.com",
+    projectId: "betutorial-48e65",
+    storageBucket: "betutorial-48e65.appspot.com",
+    messagingSenderId: "795967344088",
+    appId: "1:795967344088:web:664499f836638e3703f6ad"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 Vue.use(VueMaterial)
 
