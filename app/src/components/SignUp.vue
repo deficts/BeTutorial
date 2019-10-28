287cc06e71ef358a5c9e6ebfb2f36c7fa9add1bb
deficts <diegoar7@live.com>
2019-10-28 14:28:42 -0600

    haciendo signup funcionar

--- app/src/components/SignUp.vue
+++ app/src/components/SignUp.vue
@@ -22,19 +22,19 @@
           <div class="md-layout-item md-large-size-100 md-small-size-100">
             <md-field>
               <label for="user-email">Email</label>
-              <md-input type="email" name="user-email" id="user-email" />
+              <md-input v-model="email" type="email" name="user-email" id="user-email" />
             </md-field>
           </div>
           <div class="md-layout-item md-large-size-100 md-small-size-100">
             <md-field>
               <label for="user-password">Password</label>
-              <md-input type="password" name="user-password" id="user-password" />
+              <md-input v-model="password" type="password" name="user-password" id="user-password" />
             </md-field>
           </div>
         </div>
       </md-card-content>
       <md-card-actions>
-        <md-button type="submit" class="md-primary">Crear Usuario</md-button>
+        <md-button @click="SignUp" type="button" class="md-primary">Crear Usuario</md-button>
       </md-card-actions>
     </md-card>
   </form>
@@ -42,8 +42,28 @@
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
-  name: 'SignUp'
+  name: 'SignUp',
+  data() {
+    return{
+      email:'',
+      password:''
+    }
+
+  },
+  methods: {
+    SignUp() {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+        user => {
+          this.$router.push('/')
+        },
+        err => {
+          alert(err.message)
+        }
+      )
+    }
+  }
 }
 </script>
 
